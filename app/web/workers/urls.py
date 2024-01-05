c509fd06dc9d7ca44d6835b21699931eb3f6d5a6
a-brovchenko <bavkii081@gmail.com>
2024-01-05 13:50:10 +0200

    add - logout, register , html - AUTH_PASSWORD_VALIDATORS - custom command

--- app/web/workers/urls.py
+++ app/web/workers/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index),
+    path('registration/', registration),
+    path('workers_tree/', workers_tree),
+    path('workers_info/', workers_info),
 ]
--- app/web/workers/views.py
+++ app/web/workers/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render, redirect
+from .models import Workers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
 
 def index(request):
     return render(request, 'workers/base.html')
+
+def workers_tree(request):
+    node = Workers.objects.all()
+    return render(request, 'workers/workers_tree.html', {'nodes':node})
+
+def workers_info(request):
+    workers = Workers.objects.all()
+    return render(request, 'workers/workers_info.html', {'workers':workers})
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
